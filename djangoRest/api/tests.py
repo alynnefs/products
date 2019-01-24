bb0f59d9202dc15e5a20b96f03a0b052cdb9b136
alynnefs <alynneferreiras@gmail.com>
2019-01-24 11:20:02 -0300

    tests

--- djangoRest/api/tests.py
+++ djangoRest/api/tests.py
@@ -1,54 +1,7 @@
-from rest_framework.request import Request
-from rest_framework.test import (
-    APIRequestFactory,
-    APITestCase
-)
 from django.test import TestCase
 from model_mommy import mommy
 
 from .models import Products
-from .serializers import ProductSerializer
-
-
-class BaseViewTest(APITestCase):
-    """
-    This class creates test data
-    """
-
-    @staticmethod
-    def create_product(name="", price="", description=""):
-        if name and price and description != "":
-            Products.objects.create(name=name, price=price,
-                                    description=description)
-
-    def setUp(self):
-        # add test data
-        self.create_product("Caderno", 10.50, "Capa de bolinhas")
-        self.create_product("Livro", 20, "Usado")
-        self.create_product("Caneta", 1.50, "Tinta azul")
-        self.create_product("Lapiseira", 2, "Grafite 0.5")
-
-
-class GetAllProductsTest(BaseViewTest):
-    """
-    This class tests data creation
-    """
-    def test_get_all_products(self):
-        print("\nget products")
-
-        factory = APIRequestFactory()
-        request = factory.get('/')
-        serializer_context = {'request': Request(request)}
-
-        product = Products.objects.first()
-
-        response = ProductSerializer(instance=product,
-                                     context=serializer_context)
-
-        # fetch the data from db
-        expected = Products.objects.first()
-        serialized = ProductSerializer(expected, context=serializer_context)
-        self.assertEqual(response.data, serialized.data)    
 
 
 class TestProductsModels(TestCase):
